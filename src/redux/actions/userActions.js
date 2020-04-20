a9ec213aea345f02eced6315b471dfe8a855c0e4
Jiazi Cai <caijiazi777@gmail.com>
2020-04-20 16:05:27 -0400

    Update userActions.js

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const loginUser = (userData, history) => (dispatch) => {
     dispatch({type: LOADING_UI});
-    axios.post('/socialApp/login', userData)
+    axios.post('/login', userData)
             .then((res) => {
                 setAuthorizationHeader(res.data.token)
                 dispatch(getUserData());
