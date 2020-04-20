20311b4c3b08d6f9962802d2b1f35f70271de634
Jiazi Cai <caijiazi777@gmail.com>
2020-04-20 15:45:30 -0400

    Updates

---
+++ precache-manifest.6145f6d606bfe30cb0f67fc4312c5cf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9b5efad08e23cbd628fffdb2b3dedd",
+    "url": "/socialApp/index.html"
+  },
+  {
+    "revision": "6a2fd845020cebca6144",
+    "url": "/socialApp/static/css/main.04eb1423.chunk.css"
+  },
+  {
+    "revision": "aeeee189b528ea63b5fc",
+    "url": "/socialApp/static/js/2.c2649818.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/socialApp/static/js/2.c2649818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2fd845020cebca6144",
+    "url": "/socialApp/static/js/main.e310b37b.chunk.js"
+  },
+  {
+    "revision": "4f6d3fcd3e9517c945c5",
+    "url": "/socialApp/static/js/runtime-main.1717ce8a.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/socialApp/static/media/no-img.5a021ab9.png"
+  },
+  {
+    "revision": "38c19b99d4c87f4bcfe2d9ab77e980d6",
+    "url": "/socialApp/static/media/sociallogo.38c19b99.png"
+  }
+]);
